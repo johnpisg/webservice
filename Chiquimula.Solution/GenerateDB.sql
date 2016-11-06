1902c0571e725754ca24959af00ca8639cfeb0b9
Jhon Pierre <johnpisg@gmail.com>
2016-11-06 14:39:40 -0600

    JHON: Agregando script de generación de la BD

---
+++ Chiquimula.Solution/GenerateDB.sql
@@ -0,0 +1,33 @@
+﻿GO
+CREATE TABLE [dbo].[Imagen] (
+    [Id]      INT            IDENTITY (1, 1) NOT NULL,
+    [Nombre]  VARCHAR (500)  NOT NULL,
+    [path]    VARCHAR (2500) NOT NULL,
+    [sitioId] INT            NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+GO
+CREATE TABLE [dbo].[Video] (
+    [Id]      INT            IDENTITY (1, 1) NOT NULL,
+    [Nombre]  VARCHAR (500)  NOT NULL,
+    [path]    VARCHAR (2500) NOT NULL,
+    [sitioId] INT            NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+GO
+CREATE TABLE [dbo].[Sitio] (
+    [id]          INT            IDENTITY (1, 1) NOT NULL,
+    [nombre]      VARCHAR (500)  NOT NULL,
+    [titulo]      VARCHAR (500)  NOT NULL,
+    [descripcion] VARCHAR (4000) NOT NULL,
+    [ranking]     FLOAT (53)     NOT NULL,
+    [imagenId]    INT            NULL,
+    [horario]     VARCHAR (250)  NOT NULL,
+    [precio]      FLOAT (53)     NOT NULL,
+    [info]        VARCHAR (4000) NULL,
+    [datos]       VARCHAR (4000) NULL,
+    [masdatos]    VARCHAR (4000) NULL,
+    PRIMARY KEY CLUSTERED ([id] ASC),
+    CONSTRAINT [FK_Sitio_To_Imagen] FOREIGN KEY ([imagenId]) REFERENCES [dbo].[Imagen] ([Id])
+);
+GO
